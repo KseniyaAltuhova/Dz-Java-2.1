26011a3867968db660ab899c8f989c9385fac729
Ксения Алтухова <bogomolowakseniya@yandex.ru>
2022-02-07 21:31:22 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 10160; //цена билета
+        int amount = 20; // количество рублей для одной бонусной мили
+        int bonusmile = price / amount; //количество бонусных миль с покупки билета
+        System.out.println("За приобретенный билет начислено " + bonusmile + " бонусных миль");
+    }
+}
